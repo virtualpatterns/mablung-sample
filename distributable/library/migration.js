523ca7b4308f6ec7f73b09e36e4741989e71a625
virtualpatterns <code.virtualpatterns@gmail.com>
2020-06-25 07:39:53 -0400

    post-test

---
+++ distributable/library/migration.js
@@ -0,0 +1,24 @@
+import _URL from "url";import Path from 'path';
+import { Migration as BaseMigration } from '@virtualpatterns/mablung-sqlite-migration';
+
+const FilePath = _URL.fileURLToPath(import.meta.url);
+const FolderPath = Path.dirname(FilePath);
+
+class Migration extends BaseMigration {
+
+  constructor(path, database) {
+    super(path, database);
+  }
+
+  static createMigration(name, path = Path.normalize(`${FolderPath}/../../source/library/migration`), templatePath = Path.normalize(`${FolderPath}/../../source/library/migration/template.js`)) {
+    return super.createMigration(name, path, templatePath);
+  }
+
+  static async getMigration(...parameter) {
+    return (await Promise.all([super.getMigration(...parameter), super.getMigrationFromPath(`${FolderPath}/migration`, ['*.js'], ['template.js'], ...parameter)])).flat().sort();
+  }}
+
+
+
+export { Migration };
+//# sourceMappingURL=migration.js.map
